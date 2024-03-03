26ab5fc84ccb993d4cfea7085c4b156bc889e4fa
Mrafisyahbani77 <mrafi.syahbani@gmail.com>
2024-03-03 15:14:53 +0700

    done

--- app/Http/Controllers/Api/Kaprog/DashboardKaprogController.php
+++ app/Http/Controllers/Api/Kaprog/DashboardKaprogController.php
@@ -5,19 +5,22 @@ namespace App\Http\Controllers\Api\Kaprog;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
-use App\Models\Jurnal; // Mengimpor model Jurnal
+use App\Models\Jurnal;
 
 class DashboardKaprogController extends Controller
 {
     public function index(Request $request)
     {
-        // Mengambil total jumlah data user dengan role_id 4 dan 3
-        $totalUsers = User::whereIn('role_id', [3, 4])->count();
+        // Mengambil total jumlah data user dengan role_id 3
+        $totalUsersRole3 = User::where('role_id', 3)->count();
+
+        // Mengambil total jumlah data user dengan role_id 4
+        $totalUsersRole4 = User::where('role_id', 4)->count();
         
         // Mengambil total jumlah data jurnal
         $totalJurnals = Jurnal::count();
         
-        // Mengembalikan total jumlah data user dan total jumlah data jurnal dalam bentuk response JSON
-        return response()->json(['total_users' => $totalUsers, 'total_jurnals' => $totalJurnals]);
+        // Mengembalikan total jumlah data user (role_id 3 dan 4 terpisah) dan total jumlah data jurnal dalam bentuk response JSON
+        return response()->json(['total_users_role_3' => $totalUsersRole3, 'total_users_role_4' => $totalUsersRole4, 'total_jurnals' => $totalJurnals]);
     }
 }
